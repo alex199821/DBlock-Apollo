6fed5ac621540937bd1e2fbe4082e069ea2209a5
@alexandre174 <aupli16@freeuni.edu.ge>
2022-11-02 09:01:24 +0200

    DBlock Apollo Server Final

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-const path = require('path');
-
-const express = require('express');
-const app = express();
-const cors = require('cors');
-
 const { ApolloServer } = require("apollo-server");
 const connectDB = require("./db/connect");
 connectDB();
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 
-app.use(cors());
-
-app.use(express.static('build'));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-});
-
-server.applyMiddleware({
-  path: '/', 
-  app,
 });
 
 server.listen(process.env.PORT || 4000).then(({ url }) => {
